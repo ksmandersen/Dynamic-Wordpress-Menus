b76e6cf62b9cfab972bed01256a147f8dbfc775a
Kristian Andersen <ksm.andersen@gmail.com>
2011-09-28 21:34:51 +0200

    Added a function for finding the id for a defined menu from a post id

--- DynamicMenus.php
+++ DynamicMenus.php
@@ -13,8 +13,8 @@ Author URI: http://github.com/ksmandersen/
 require_once 'meta-box.php';
 
 // Get the available menu's registed in Wordpress Menu interface
-function get_menus(){
-    $r = array(-1 => "");
+function get_all_menus(){
+    $r = array(0 => "");
     $menus = wp_get_nav_menus();
 		if(is_array($menus) && count($menus) > 0) {
 			foreach($menus as $key => $menu) {
@@ -24,6 +24,48 @@ function get_menus(){
 		}
     return $r;
 }
+
+function get_menu_id($post_id) {
+	// Return if not given a valid page id.
+	if($post_id <= 0)
+		return;
+		
+	// Get the post for the page id
+	$post = get_post($post_id);
+	if($post == null) // Return if post is invalid
+		return;
+		
+	// Get the menu set for the current page
+	$menu_id = get_post_meta($post->ID, 'dpm_page-menu-id', true);
+	
+	// If no menu is set for the current page then go over all parents until a menu
+	// is found or there are no more parents.
+	if($menu_id <= 0) {
+		do {
+			$post = get_post($post->post_parent); // Get post for parent page
+			if($post == null)
+				return;
+				
+			$menu_id = get_post_meta($post->ID, 'dpm_page-menu-id', true);
+			
+		} while($menu_id <= 0 && $post->post_parent > 0);
+	}
+	
+	return $menu_id;
+}
+
+// function get_menu_id($post) {
+// 	// Get the menu id for the current post (page).
+// 	$menu_id = get_post_meta($post->ID, 'dpm_page-menu-id', true);
+// 	
+// 	// If no menu id is defined for the current post then
+// 	// keep on searching in the parents.
+// 	do {
+// 		$menu_id = get_post_meta($post->post_parent, 'dpm_page-menu-id', true);
+// 	} while($menu_id <= 0 && $parent != 0);
+// 	
+// 	return $menu_id;
+// }
 
 // Register meta box for page
 function page_meta() {
@@ -37,7 +79,7 @@ function page_meta() {
 				'name'		=> 'Menu',
 				'id'			=> 'dpm_page-menu-id',
 				'type'		=> 'select',
-				'options'	=> get_menus()
+				'options'	=> get_all_menus()
 			)
 		)
 	);
